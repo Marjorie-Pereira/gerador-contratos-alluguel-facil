d3b7eaf2a8058ab2c17c09c0fffb41e30f23f8ee
Marjorie-Pereira <marjorie300904@gmail.com>
2026-07-06 18:01:52 -0300

    Update import

--- app/ui/components/FormSection.tsx
+++ app/ui/components/FormSection.tsx
@@ -9,36 +9,9 @@ import { cn } from "@/lib/utils";
 import { FieldWrapper } from "@/app/ui/components/FieldWrapper";
 import { FormSectionProps } from "@/types/formTypes";
 import { FormSelect } from "./FormSelect";
+import { FormInput } from "./FormInput";
 
-const FormInput: FC<{
-  id: string;
-  label: string;
-  required?: boolean;
-  inputProps?: InputHTMLAttributes<HTMLInputElement>;
-  placeholder?: string;
-  className?: string;
-}> = ({ id, label, required, inputProps, placeholder, className }) => {
-  return (
-    <FieldWrapper
-      id={id}
-      label={label}
-      required={required}
-      className={className}
-    >
-      <input
-        id={id}
-        required={required}
-        placeholder={placeholder}
-        {...inputProps}
-        className={cn(
-          "w-full p-4 text-base border border-gray-300 bg-zinc-100 rounded-lg",
-          "placeholder-gray-400 focus:ring-2 focus:ring-amber-500 focus:border-amber-500 outline-none transition-all",
-          inputProps?.className,
-        )}
-      />
-    </FieldWrapper>
-  );
-};
+
 
 const ImovelDataSection: FC<FormSectionProps> = ({
   icon,
