e1287fc6e85f80550e28389c246954d57ec588e3
Marjorie-Pereira <marjorie300904@gmail.com>
2026-07-22 21:54:52 -0300

    Make a link to go back

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { ArrowLeft, Download } from "lucide-react";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const PreviewPDFDynamic = dynamic(
   () => import("@/app/ui/components/PreviewPDFDocument"),
@@ -19,7 +20,7 @@ export default function PreviewPDFPage() {
         <Download /> Download PDF
       </button>
       <button className="absolute top-3 left-8 bg-yellow-600 p-4 flex items-center gap-2 hover:opacity-80 hover:cursor-pointer">
-        <ArrowLeft /> Voltar
+        <ArrowLeft /> <Link href={"/"}>Voltar</Link>
       </button>
 
       <PreviewPDFDynamic url={pdfUrl} />
